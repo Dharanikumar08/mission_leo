17d0904357864210a5914b94e77077a9c3d43df5
Dharani Kumar <dharanikumarpoturaju@gmail.com>
2026-08-04 21:48:46 +0530

    Mission LEO Day 1 - First C Program

---
+++ CODING/Day01/hello_mission_leo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("=================================\n");
+    printf("        MISSION LEO\n");
+    printf("=================================\n");
+    printf("Name   : Dharani Kumar\n");
+    printf("Goal   : IAS Officer\n");
+    printf("Degree : B.Tech CSE (AI)\n");
+    printf("Mission: Build an Extraordinary Resume\n");
+    printf("=================================\n");
+    return 0;
+}
