0f81c8606ebe84ebe641789f231e3b13b3a91c15
Dharani Kumar <dharanikumarpoturaju@gmail.com>
2026-08-05 11:18:39 +0530

    Mission LEO Day 2 - Variables and Calculator

---
+++ CODING/Day02/variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 21;
+    float cgpa = 8.75;
+    char grade = 'A';
+
+    printf("===== MISSION LEO DAY 2 =====\n");
+    printf("Age   : %d\n", age);
+    printf("CGPA  : %.2f\n", cgpa);
+    printf("Grade : %c\n", grade);
+
+    return 0;
+}
